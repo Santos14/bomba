0fffc3be6d7249d88805a054df291a86bf4ec4a5
SantosR14 <sergio.12.educando@gmail.com>
2016-11-23 09:52:33 -0500

    ...

--- application/views/layout/foother.php
+++ application/views/layout/foother.php
@@ -10,5 +10,18 @@
         <script src="<?= base_url(); ?>js/bootstrap.min.js" type="text/javascript"></script>
         <!-- AdminLTE App -->
         <script src="<?= base_url(); ?>js/AdminLTE/app.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+
+                $("#mas").click(function(e){
+                    e.preventDefault();
+                    $("#alternativas").append('<div class="form-group">'+
+                    '<div class="col-lg-7"><input type="text" class="form-control" id="inputPassword" placeholder="Alternativa">'+
+                    '</div><div class="col-lg-2"><div class="radio"><label>'+
+                    '<input type="radio" name="opciones" id="opciones_1" value="opcion_1" checked>'+
+                    'Correcto</label></div></div></div>');
+                });
+            });
+        </script>
     </body>
 </html>
--- application/views/pregunta/index.php
+++ application/views/pregunta/index.php
@@ -1,32 +1,52 @@
 
-<form role="form" action="" method="post">
 <input type="hidden" value="1" name="guardar">    
 <div class="row"><!-- Formulario de la ruta Viaje -->
     <div class="col-md-12">
         <div class="box box-primary">
             <div class="box-header">
                 <i class="fa fa-book"></i>
-                <h3 class="box-title">Informacion General</h3>
+                <h3 class="box-title">Agregar Preguntas</h3>
             </div>
             <div class="box-body">                
                 <div class="row" >   
                     <div class="col-md-10">                           
-                        <div class="box-body table-responsive">
-                            <table  id="tab" class="table table-bordered table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Id</th>
-                                        <th>Nombre</th>
-                                        <th>Descripcion</th>
-                                        <th>Estado</th>
-                                        <th colspan="2">Acciones</th>
-                                    </tr>
-                                </thead>  
-                            </table>
-                            <div class="btn-group">
-                                    <a class="btn btn-primary k-button" id="nuevo_modal" data-toggle="modal" data-target="#modal_form"><i class="fa  fa-plus"></i> Nuevo</a>
+
+                        <form class="form-horizontal" role="form">
+                          <div class="form-group">
+                            <div class="col-lg-10">
+                              <input type="text" class="form-control" id="inputPassword" placeholder="Descripcion">
                             </div>
-                        </div>
+                          </div>
+                          <div id='alternativas'>
+                          <div class="form-group">
+                            <div class="col-lg-7">
+                              <input type="text" class="form-control" id="inputPassword" placeholder="Alternativa">
+                              
+                            </div>
+                            <div class="col-lg-2">
+                                <div class="radio">
+                                  <label>
+                                    <input type="radio" name="opciones" id="opciones_1" value="opcion_1" checked>
+                                    Correcto
+                                  </label>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                              <button class="btn btn-info btn-flat"  type="button" id="mas">+</button>
+                            </div>
+                          </div>
+
+                        
+
+                      </div>
+                          
+                        </form>
+
+
+
+
+
+
                     </div>
                 </div> 
             </div>
@@ -35,63 +55,13 @@
 </div>
 <button class="btn btn-info btn-flat" type="button" id="submit_form_servicio">Guardar</button>
 <button class="btn btn-info btn-flat" onclick="prueba()" type="button">prueba</button>
-</form>
 
-
-
-<div class="modal fade" id="modal_form" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title"><i class="fa fa-gear"></i> Formulario Tipo Pieza</h4>
-            </div>
-            <form role="form" action="" method="post">
-                <div class="modal-body">
-                    <div id="msg" class="form-group has-warning"></div>
-                    <div class="form-group">
-                        <label for="id">Identificador</label>
-                        <input type="text" class="form-control" id="id" name="id" readonly="readonly" >
-                    </div>  
-                    <div class="form-group">
-                        <label for="nombre">Nombre</label>
-                        <input type="text" required class="form-control" id="nombre" name="nombre" placeholder="Ingrese Nombre" onkeypress="return soloLetras(event)">
-                    </div>
-                    <div class="form-group">
-                        <label for="descripcion">Descripcion</label>
-                        <input type="text" required class="form-control" id="descripcion" name="descripcion" placeholder="Ingrese Descripcion" onkeypress="return soloLetras(event)">
-                    </div>
-                </div>
-                <div class="modal-footer clearfix">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Cancelar</button>
-                    <button type="button" id='submit_form' class="btn btn-primary pull-left"><i class="fa fa-check"></i> Guardar</button>
-                </div>
-            </form>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->                
-
-<div class="modal fade" id="modal_delete" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title"><i class="fa fa-exclamation-circle"></i> Alerta eliminar </h4>
-            </div>
-            <form role="form" action="" method="post">
-                <input type="hidden" id='id_dato_eliminar'></input>
-               
-                <div class="modal-body" >
-                    <div class="alert alert-danger alert-dismissable">
-                        <i class="fa fa-warning"></i>
-                        <h4>Estas seguro que desea eliminar el dato?</h4><h4 id="desc_dato_eliminar"></h4>
-                    </div>
-                </div>
-                <div class="modal-footer clearfix">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Cancelar</button>
-                    <button type="button" id="delete_click" class="btn btn-primary pull-left"><i class="fa fa-check"></i> Aceptar</button>
-                </div>
-            </form>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal --> 
+<script type="text/javascript">
+    function agregar(){
+        $("#alternativas").append('<div class="form-group">'+
+            '<div class="col-lg-7"><input type="text" class="form-control" id="inputPassword" placeholder="Alternativa">'+
+            '</div><div class="col-lg-2"><div class="radio"><label>'+
+            '<input type="radio" name="opciones" id="opciones_1" value="opcion_1" checked>'+
+            'Correcto</label></div></div></div>');
+    }
+</script>
